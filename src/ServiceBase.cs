298992a5cca4919bb692c0e1e70064a03e175af0
karl <karl.statz@gmail.com>
2017-01-28 21:52:15 -0600

    making the default wait on the retry closer to the default ttl

--- src/ServiceBase.cs
+++ src/ServiceBase.cs
@@ -5,6 +5,6 @@ namespace tex.net
     public abstract class ServiceBase
     {
         public virtual int RetryCount => 5;
-        public virtual TimeSpan RetryDelay => new TimeSpan(0, 0, 0, 0, 100);
+        public virtual TimeSpan RetryDelay => new TimeSpan(0, 0, 0, 0, 500);
     }
 }
